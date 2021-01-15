8c37cb19a8f44b403d6f00c44d2ce01dae3b4e5d
Ifeadewumi <61970764+Ifeadewumi@users.noreply.github.com>
2021-01-15 01:25:13 +0100

    development dependencies

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,9 +1,15 @@
-var express = require('express');
-var router = express.Router();
+/* ES6 Version*/
+import express from 'express';
+
+const indexRouter = express.Router();
+
+// var express = require('express');
+// var router = express.Router();
 
 /*  ##Comments## */
-router.get('/', function(req, res, next) {
-  return res.status(200).json({ message: 'Welcome to Express API template'});
-});
+indexRouter.get('/', (req, res) =>
+  res.status(200).json({ message: 'Welcome to Express API template'})
+);
 
-module.exports = router;
+// module.exports = router;
+export default indexRouter;
